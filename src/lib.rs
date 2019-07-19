0138ef7d6a54ee95f33795368b52527531341e67
scullionw <williamscullion@gmail.com>
2019-07-19 16:21:33 -0400

    seperated into benchtest, bench, and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,39 @@
 #[macro_export]
 macro_rules! benchtest {
     ($($name:ident: $compute:expr => $ans:expr),*) => (
+        mod benchtests {
+            #[cfg(test)]
+            mod tests {
+                use super::super::*;
+                $(
+                    #[test]
+                    fn $name() {
+                        assert_eq!($compute, $ans);
+                    }
+                )*
+            }
+
+            #[cfg(test)]
+            mod benches {
+                use super::super::*;
+                extern crate test;
+                use self::test::Bencher;
+                $(
+                    #[bench]
+                    fn $name(b: &mut Bencher) {
+                        b.iter(|| $compute);
+                    }
+                )*
+
+            }
+        }
+
+    )
+}
+
+#[macro_export]
+macro_rules! test {
+    ($($name:ident: $compute:expr => $ans:expr),*) => (
         #[cfg(test)]
         mod tests {
             use super::*;
@@ -11,20 +44,23 @@ macro_rules! benchtest {
                 }
             )*
         }
+    )
+}
 
+#[macro_export]
+macro_rules! bench {
+    ($($name:ident: $compute:expr),*) => (
         #[cfg(test)]
         mod benches {
             use super::*;
             extern crate test;
             use self::test::Bencher;
-
             $(
                 #[bench]
                 fn $name(b: &mut Bencher) {
                     b.iter(|| $compute);
                 }
             )*
-
         }
     )
 }
